319844acf9bd2ceeb7fe5a58c5eabeb672ecb313
Michael Smith <michaelsmith19951@yahoo.com>
2018-08-13 11:20:10 -0400

    Added additional tests for test_if_dash_is_removed and test_that_letters_are_removed

--- tdd_isbn.rb
+++ tdd_isbn.rb
@@ -5,6 +5,7 @@ class TestIsbn < Minitest::Test
 
   def test_that_dash_is_removed
         assert_equal("12345", isbn_dash("1-2-3-4-5"))
+        assert_equal("12345", isbn_dash("-1-2-3-4-5-"))
     end
   def test_that_characters_are_removed
   		assert_equal("2345", isbn_characters("!2345"))
@@ -13,5 +14,6 @@ class TestIsbn < Minitest::Test
   def test_that_letters_are_removed
   		assert_equal("12345", isbn_letters("1a2345"))
   		assert_equal("877195869x", isbn_letters("877195869x"))
+  		assert_equal("877195869X", isbn_letters("877195869X"))
   	end
 end
