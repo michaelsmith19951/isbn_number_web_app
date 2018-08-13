2e1fef755bfc025b3af1ae5d460c523f4c59e515
Michael Smith <michaelsmith19951@yahoo.com>
2018-08-13 11:15:17 -0400

    Improved existing tests

--- isbn_functions.rb
+++ isbn_functions.rb
@@ -1,18 +1,26 @@
-def isbn_dash(num)
-    num.to_s
-    num = num.tr('-', '')
-    num = num.tr(' ', '')
-    num
+def isbn_dash(isbn)
+    isbn.to_s
+    isbn = isbn.tr('-', '')
+    isbn = isbn.tr(' ', '')
+    isbn
 end
-def isbn_letters(alphabet)
-	alphabet.to_s
-	alphabet = alphabet.tr("a..z", '')
-	alphabet = alphabet.tr(' ', '')
-	alphabet
+def isbn_characters(isbn)
+	isbn2 = isbn_dash(isbn)
+	isbn2.to_s
+	isbn2 = isbn2.tr("~!@#$%^&*+/", '')
+	isbn2 = isbn2.tr(' ', '')
+	isbn2
 end
-def isbn_characters(characters)
-	characters.to_s
-	characters = characters.tr("~!@#$%^&*+/", '')
-	characters = characters.tr(' ', '')
-	characters
+def isbn_letters(isbn)
+	isbn2 = isbn_characters(isbn)
+	isbn2.to_s
+	if isbn2.length == 10 && isbn2[-1] == "x"
+		isbn2
+	elsif isbn2.length == 10 && isbn2[-1] == "X"
+		isbn2
+	else
+		isbn2 = isbn2.tr("a..z", '')
+		isbn2 = isbn2.tr(' ', '')
+		isbn2
+	end
 end
--- tdd_isbn.rb
+++ tdd_isbn.rb
@@ -6,10 +6,12 @@ class TestIsbn < Minitest::Test
   def test_that_dash_is_removed
         assert_equal("12345", isbn_dash("1-2-3-4-5"))
     end
-  def test_that_letters_are_removed
-  		assert_equal("", isbn_letters("a..z"))
-  	end
   def test_that_characters_are_removed
-  		assert_equal("", isbn_characters("~!@#$%^&*+/"))
+  		assert_equal("2345", isbn_characters("!2345"))
+  		assert_equal("2345", isbn_characters("!2-345"))
+  	end
+  def test_that_letters_are_removed
+  		assert_equal("12345", isbn_letters("1a2345"))
+  		assert_equal("877195869x", isbn_letters("877195869x"))
   	end
 end
