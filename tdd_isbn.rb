e3c184ecad4ec8df0e77fd84873995c3da94b7c9
Michael Smith <michaelsmith19951@yahoo.com>
2018-08-13 10:01:39 -0400

    Second test that passed, test_that_dash_is_removed

--- tdd_isbn.rb
+++ tdd_isbn.rb
@@ -4,7 +4,7 @@ require_relative "isbn_functions.rb"
 class TestIsbn < Minitest::Test
 
   def test_that_dash_is_removed
-        assert_equal("1-2345", isbn_dash("1-2-3-4-5"))
+        assert_equal("12345", isbn_dash("1-2-3-4-5"))
         # assert_equal("12345", isbn_dash("1 - 23-4 5"))
         # assert_equal("12345", isbn_dash("1  -  2  -  3  -  4  -  5"))
     end
